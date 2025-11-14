1118271d138a701b600edb86874c35e47b928904
JaydJohns <97568021+JaydJohns@users.noreply.github.com>
2025-11-13 21:32:50 -0500

    dwd

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+set -e
+sudo sh -c 'echo "codespace ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/codespace'
+sudo apt-get update
+sudo apt-get install -y postgresql postgresql-contrib
 sudo service postgresql start
 sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'postgres';"
